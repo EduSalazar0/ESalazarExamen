e8f5334432dce0322894b205532368d2730a88f5
EduSalazar0 <edutebansalazar95@gmail.com>
2025-01-20 07:37:09 -0500

    Se incluyo etiquetas referentes a SQLite, una clase PaisRepository, PaisService y se movio de la carpeta view la clase creada.

---
+++ ESalazarExamen/Models/Pais.cs
@@ -0,0 +1,24 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESalazarExamen.Models
+{
+    [SQLite.Table("Pais")]
+    public class Pais
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [SQLite.Column("Nombre")]
+        public string Nombre { get; set; }
+        [SQLite.Column("Region")]
+        public string region { get; set; }
+        [SQLite.Column("Link")]
+        public string linkMaps { get; set; }
+
+    }
+}
---
+++ ESalazarExamen/Repositories/PaisRepository.cs
@@ -0,0 +1,45 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESalazarExamen.Models;
+
+namespace ESalazarExamen.Repositories
+{
+    public class PaisRepository
+    {
+        string _dbPath;
+
+        public string StatusMessage { get; set; }
+
+        private SQLiteConnection conn;
+
+        private void Init()
+        {
+            if (conn == null)
+                return;
+            conn = new SQLiteConnection(_dbPath);
+            conn.CreateTable<Pais>();
+        }
+
+        public PaisRepository(string dbPath)
+        {
+            _dbPath = dbPath;
+        }
+
+        public List<Pais> GetAllPaises()
+        {
+            try
+            {
+                Init();
+                return conn.Table<Pais>().ToList();
+            } catch (Exception ex)
+            {
+                StatusMessage = $"Error al listar la informacion. DEtalle: {ex.Message}";
+            }
+            return new List<Pais>();
+        }
+    }
+}
