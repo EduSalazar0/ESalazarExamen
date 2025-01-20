c955a6b4fdfacb952d20bcc877d18229cd881d80
EduSalazar0 <edutebansalazar95@gmail.com>
2025-01-20 07:18:58 -0500

    Creacion de carpetas views,Services,Models y Repositories

---
+++ ESalazarExamen/Views/Pais.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ESalazarExamen.Views
+{
+    public class Pais
+    {
+        public string Nombre { get; set; }
+        public string region { get; set; }
+        public string linkMaps { get; set; }
+        public 
+
+    }
+}
